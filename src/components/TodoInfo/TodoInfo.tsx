98cc6a580dbfc2f575df490f79f844313ff21ac1
Nikita Zaluzhnyi <nikita@Nikitas-MacBook-Air.local>
2025-07-26 14:06:36 +0200

    first try

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,93 @@
-export const TodoInfo = () => {};
+import { Todo, User } from '../TodoList';
+import React, { useState } from 'react';
+
+type Props = {
+  users: User[];
+  createNewTodo: (param: Todo[]) => void;
+  todos: Todo[];
+};
+
+export const TodoInfo: React.FC<Props> = ({ users, createNewTodo, todos }) => {
+  const [userSelect, setUserSeclect] = useState('');
+  const [hasUserSelectError, setHasUserSelectError] = useState(false);
+  const [title, setTitle] = useState('');
+  const [hasTitleError, setHasTitleError] = useState(false);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setHasTitleError(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newTodo = {
+      id: Math.max(...todos.map(t => t.id), 0) + 1,
+      title: title.trim(),
+      completed: false,
+      userId: users.find(user => user.name === userSelect)?.id || 0,
+      user: users.find(user => user.name === userSelect),
+    };
+
+    const isTitleEmpty = !title.trim();
+    const isUserNotSelected = !userSelect;
+
+    setHasTitleError(isTitleEmpty);
+    setHasUserSelectError(isUserNotSelected);
+
+    if (isTitleEmpty || isUserNotSelected) {
+      return;
+    }
+
+    createNewTodo([...todos, newTodo]);
+
+    setTitle('');
+    setUserSeclect('');
+  };
+
+  return (
+    <>
+      <h1>Add todo form</h1>
+
+      <form action="/api/todos" method="POST" onSubmit={handleSubmit}>
+        <div className="field">
+          <input
+            type="text"
+            data-cy="titleInput"
+            onChange={handleTitleChange}
+            value={title}
+            placeholder="Enter a title"
+          />
+          {hasTitleError && <span className="error">Please enter a title</span>}
+        </div>
+
+        <div className="field">
+          <select
+            data-cy="userSelect"
+            value={userSelect}
+            onChange={event => {
+              setUserSeclect(event.target.value);
+              setHasUserSelectError(false);
+            }}
+          >
+            <option value="" disabled>
+              Choose a user
+            </option>
+            {users.map(user => (
+              <option key={user.id} value={user.name}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+
+          {hasUserSelectError && (
+            <span className="error">Please choose a user</span>
+          )}
+        </div>
+
+        <button type="submit" data-cy="submitButton">
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,41 @@
-export const TodoList = () => {};
+export type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+type Props = {
+  todos: Todo[];
+  users: User[];
+};
+
+export const TodoList: React.FC<Props> = ({ todos, users }) => {
+  return (
+    <section className="TodoList">
+      {todos.map((todo, index) => (
+        <article
+          key={index}
+          data-id={todo.id}
+          className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}
+        >
+          <h2 className="TodoInfo__title">{todo.title}</h2>
+
+          <a
+            className="UserInfo"
+            href={`mailto:${users.find(user => user.id === todo.userId)?.email}`}
+          >
+            {users.find(user => user.id === todo.userId)?.name}
+          </a>
+        </article>
+      ))}
+    </section>
+  );
+};
